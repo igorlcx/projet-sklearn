ce0dd584ee52757313a47638990579bb7c555bea
Igor Lacroix <igor.lacroix@ensae.fr>
2026-01-11 15:32:38 +0100

    add plot mais pas matplotlib

--- src/train_model.py
+++ src/train_model.py
@@ -4,3 +4,9 @@ import yfinance as yf
 nasdaq = yf.download('^IXIC', start='2020-01-01', end='2025-01-01')
 nasdaq.head()
 print(nasdaq.head())
+
+nasdaq['Close'].plot(figsize=(12, 6))
+plt.title('NASDAQ Index (2020-2025)')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
