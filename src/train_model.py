8f3d38a85370da01c6a090d0745cd66e5f7f1da3
Igor Lacroix <igor.lacroix@ensae.fr>
2026-01-12 09:58:27 +0100

    "ajout des stocks"

--- src/train_model.py
+++ src/train_model.py
@@ -6,9 +6,24 @@ nasdaq = yf.download('^IXIC', start='2020-01-01', end='2025-01-01')
 nasdaq.head()
 print(nasdaq.head())
 
-nasdaq['Close'].plot(figsize=(12, 6))
-plt.title('NASDAQ Index (2020-2025)')
+# Télécharger les données pour Apple, Google, Meta et Nvidia
+stocks_data = yf.download(['AAPL', 'GOOGL', 'META'], start='2020-01-01', end='2025-01-01')
+
+# Extraire les données de clôture
+stocks = stocks_data['Close']
+stocks.columns = ['Apple', 'Google', 'Meta']
+
+# Ajouter le NASDAQ
+stocks['NASDAQ'] = nasdaq['Close']
+norme = stocks.iloc[0]  # Première ligne 
+stocks_normalized = stocks / norme
+print(stocks_normalized.head())
+
+# Tracer tous les stocks sur le même graphe
+stocks_normalized.plot(figsize=(12, 6))
+plt.title('Stocks + NASDAQ (2020-2025)')
 plt.xlabel('Date')
 plt.ylabel('Price')
+plt.legend()
 plt.show()
-print("test actuel")
+
